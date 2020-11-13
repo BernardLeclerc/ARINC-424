9ff1b6a52eb411ea515518d79f682624ecdb7cbb
Bernard Leclerc <bernard.leclerc.1960@gmail.com>
2020-11-13 00:02:33 -0500

    First draft xs.h to map XSD types to C++ types

---
+++ inc/xs.h
@@ -0,0 +1,15 @@
+#pragma once
+
+/// This is a helper file with the definitions of XML Schema Types.
+
+#include <string>
+
+namespace xs
+{
+  /// 
+  typedef std::string ID;
+
+  /// 
+  typedef std::string IDREF;
+
+} // namespace xs
