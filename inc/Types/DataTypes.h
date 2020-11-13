79c1a0eda79a56d2afddaafedd6657f40ffa4b30
Bernard Leclerc <bernard.leclerc.1960@gmail.com>
2020-11-12 23:54:47 -0500

    Continued adding enumerations and data types

--- inc/Types/DataTypes.h
+++ inc/Types/DataTypes.h
@@ -8,21 +8,82 @@ namespace Arinc424
 {
   namespace Type
   {
+    /// The “ATA/IATA” field contains the Airport/Heliport ATA/IATA designator code to which the data contained in the record relates.
+    typedef std::string ATAIATADesignator;
+
+    /// The “Core Identifier” field contains the character-name-code, or other series of characters, with which the object is identified.
+    /// This includes Waypoint Identifiers, VHF NAVAID Identifiers, NDB NAVAID identifier, Airport Identifiers, Runway Identifiers, and other objects.
     typedef std::string CoreIdentifier;
+
+    /// The "Cycle Date" field identifies the calendar period in which the record was added to the file or last revised.
+    /// A change in any ARINC 424 field, except Dynamic Magnetic Variation, Frequency Protection, Continuation Record Number and File Record Number, requires a cycle date change.
+    /// The cycle date will not change if there is no change in the data.
+    /// The first two digits of the field contain the last two digits of the year in which the addition or revision was made.
+    /// The last two digits contain the numeric identity of the 28-day data update cycle during which the change occurred.
+    /// Each calendar year contains 13 such cycles; however, on rare occasions 14 cycles will be encountered.
+    /// Note: This field does not indicate changes on elements/attributes of type ID or IDREF.
     typedef std::string CycleDate;
+
+    /// The “Customer Area Code” field permits the categorization of standard records by geographical area and of tailored records by the airlines for whom they are provided in the master file.
+    /// Several record types do not adhere to the established geographical boundaries.
+    /// There is no “AREA” in such records.
     typedef std::string CustomerCode;
+
+    /// The “Daylight” Time Indicator” field is used to indicate if the airport observes Daylight or Summer time when such time changes are in effect for the country or state the airport resides in.
+    typedef bool DaylightIndicator;
+
+    /// Elevation of the respective feature.
+    /// Restricted to the range -1500 to 20000
+    typedef int Elevation;
+
+    /// This element provides the 2 character code to identify a state
+    typedef std::string IcaoCode;
+
+    /// The “IFR Capability” field indicates if the Airport/Heliport has any published Instrument Approach Procedures.
+    typedef bool IFRCapability;
+
+    /// The “Name” field defines the name commonly applied to the navigation entity defined in the record.
+    /// The field is restricted to a maximum of 50 characters.
+    typedef std::string Name;
+
+    ///
     typedef std::string NotesText;
+
+    ///
     typedef std::string PointReference;
 
+    ///
     typedef unsigned int LongestRunway;
 
-    union CustAreaCode
+    /// This Class represents the "Alpha" character type of the Arinc 424 data.
+    class Alpha
     {
-      Enum::AreaCode areaCode;
-      CustomerCode customerCode;
+      public:
+        Alpha();
+        virtual ~Alpha();
+
+      private:
     };
 
-    /// \todo Consider defining a class to implement the restriction in the range of valid integers: from -2000 to 99999
+    /// The “Customer Area Code” field permits the categorization of standard records by geographical area
+    /// and of tailored records by the airlines for whom they are provided in the master file.
+    /// Several record types do not adhere to the established geographical boundaries.
+    /// There is no “AREA” in such records.
+    class CustAreaCode : public Alpha
+    {
+      public:
+        CustAreaCode();
+        virtual ~CustAreaCode();
+
+      private:
+        Enum::AreaCode areaCode;
+        CustomerCode customerCode;
+    };
+
+    /// The all-numeric altitude field will contain altitudes in feet with a resolution of one foot.
+    /// If isFlightLevel is true, this element represents altitude as a flight level.
+    /// The contents of this field only contain the numerical portion of the flight level expressed in hundreds of feet (e.g., 120, 1, or 10).
+    /// Restricted to the range -2000 to 99999.
     typedef int AltitudeValue;
 
     class Altitude
@@ -51,5 +112,147 @@ namespace Arinc424
         bool isNotSpecified;
         bool isUnlimited;
     };
+
+    /// This field specifies frequency and unit of measure of the frequency information.
+    class Frequency
+    {
+      public:
+        Frequency();
+        ~Frequency();
+
+      private:
+        Enum::FreqUnitOfMeasure freqUnitOfMeasure;
+        float frequencyValue;
+    };
+
+    /// This is an abstract class that is the parent class for degrees.
+    typedef int LatLongDegree;
+
+    /// Contains degrees of a coordinate as an integer from 0 to 90.
+    typedef LatLongDegree LatDegree;
+
+    /// Contains degrees of a coordinate as an integer from 0 to 180.
+    typedef LatLongDegree LongDegree;
+
+    /// Contains minutes of a coordinate as an integer from 0 to 60.
+    typedef int Minute;
+
+    /// Contains seconds of a coordinate as an integer from 0 to 60.
+    typedef int Second;
+
+    /// Contains hundredths of a coordinate as an integer from 0 to 99.
+    typedef int HundredthSecond;
+
+    /// The “Latitude” field contains the latitude of the navigational feature identified in the record.
+    /// The latitude is a complex type that consists of children elements that contain scaled integers.
+    class Latitude
+    {
+      public:
+        Latitude();
+        virtual ~Latitude();
+
+      private:
+        LatDegree deg;
+        Minute min;
+        Second sec;
+        HundredthSecond hSec;
+        Enum::NorthSouth northSouth;
+
+        /// This field is optional for convenience in data processing.
+        /// The value of this field is not authoritative.
+        double decimalDegreesLatitude;
+    };
+
+    /// The Longitude field contains the longitude of the geographic position of the navigational feature identified in the record.
+    class Longitude
+    {
+      public:
+        Longitude();
+        ~Longitude();
+
+      private:
+        LongDegree deg;
+        Minute min;
+        Second sec;
+        HundredthSecond hSec;
+        Enum::EastWest eastWest;
+
+        /// This field is optional for convenience in data processing.
+        /// The value of this field is not authoritative.
+        double decimalDegreesLongitude;
+    };
+
+    /// Latitude and Longitude details of this point.
+    class Location
+    {
+      public:
+        Location();
+        virtual ~Location();
+
+      private:
+        Latitude latitude;
+        Longitude longitude;
+    };
+
+    /// The “Magnetic Variation” field specifies the angular difference between True North and Magnetic North at the location defined in the record.
+    /// “Dynamic Magnetic Variation” is a computer model derived value and takes location and date into consideration.
+    /// For the “Station Declination” used in some record types, refer to Section 5.66.
+    typedef double MagneticVariationValue;
+
+    /// The “Magnetic Variation” field specifies the angular difference between True North and Magnetic North at the location defined in the record.
+    /// “Dynamic Magnetic Variation” is a computer model derived value and takes location and date into consideration.
+    /// For the “Station Declination” used in some record types, refer to Section 5.66.
+    class MagneticVariation
+    {
+      public:
+        MagneticVariation();
+        ~MagneticVariation();
+
+        private:
+          Enum::MagneticVariationEWT magneticVariation;
+          MagneticVariationValue magneticVariationValue;
+    };
+
+    ///
+    class NdbNavaidClass
+    {
+      public:
+        NdbNavaidClass();
+        ~NdbNavaidClass();
+
+      private:
+        bool isBFORequired; ///< BFO Operation.  Use of Beat Frequency Oscillator type of equipment is required to received an aural identification signal.
+        Enum::NdbNavaidCoverage ndbNavaidCoverage;
+        Enum::NdbNavaidIfMarkerInfo ndbNavaidIfMarker;
+        Enum::NdbNavaidType ndbNavaidType;
+        Enum::NavaidWeatherInfo ndbNavaidWeatherInfo;
+        bool isNoVoice;
+    };
+
+    /// The Speed Limit field defines a speed, expressed in Knots, Indicated (K.I.A.S.), for a fix in a terminal procedure or for an airport or heliport terminal environment.
+    /// Restricted to less than 1000.
+    typedef unsigned int SpeedLimit;
+
+    /// The standard time zone system is based on the division of world into 24 zones, each of 15 degrees longitude.
+    /// The “zero” time zone is entered at Greenwich meridian with longitudes 7 degrees, 30 minutes West and 7 degrees, 30 minutes east, and there is no difference in the standard time of this time zone and Greenwich Mean Time.
+    /// Time zones are designated by an integer offset from Greenwich Mean Time, which represets the time difference from GMT in hours, and a minute offset for non-standard time zone offsets.
+    /// See ARINC 424 5.178 for further explanation.
+    class TimeZone
+    {
+      public:
+        TimeZone();
+        ~TimeZone();
+
+      private:
+        int hourOffset; ///< From -14 to +12
+        int minuteOffset;
+    };
+
+    /// The “Transition Altitude” field defines the altitude in the vicinity of an airport or heliport at or below which the vertical position of an aircraft is controlled by reference to altitudes (MSL).
+    /// The “Transition Level” field defines the lowest flight level available for use above the transition altitude.
+    /// Aircraft descending through the transition layer will use altimeters set to local station pressure, while departing aircraft climbing through the layer will be using standard altimeter setting (QNE) of 29.92 inches of mercury, 1013.2 millibars or 1013.2 hectopascals.
+    /// Restricted to a total of 5 digits.
+    typedef AltitudeValue TransitionAltitude;
+
   } // namespace Type
 } // namespace Arinc424
--- inc/Types/Enumerations.h
+++ inc/Types/Enumerations.h
@@ -31,5 +31,243 @@ namespace Arinc424
       Soft,
       Water
     };
-  }
+
+    enum class DatumCode
+    {
+      Undefined,
+      ADA,
+      AFG,
+      AIA,
+      AIN,
+      AMA,
+      ANO,
+      ANS,
+      ARF,
+      ARS,
+      ASC,
+      ASM,
+      ASQ,
+      ATF,
+      AUA,
+      AUG,
+      BAT,
+      BER,
+      BID,
+      BOO,
+      BUR,
+      CAC,
+      CAI,
+      CAO,
+      CAP,
+      CAZ,
+      CCD,
+      CGE,
+      CHI,
+      CHU,
+      COA,
+      DAL,
+      DAN,
+      DID,
+      DOB,
+      EAS,
+      ENW,
+      EST,
+      EUR,
+      EUS,
+      FAH,
+      FLO,
+      FOT,
+      GAA,
+      GAN,
+      GEO,
+      GIZ,
+      GRA,
+      GRX,
+      GSE,
+      GUA,
+      HEN,
+      HER,
+      HIT,
+      HJO,
+      HKD,
+      HTN,
+      IBE,
+      IDN,
+      IGF,
+      IND,
+      INF,
+      ING,
+      INH,
+      IRL,
+      ISG,
+      IST,
+      JOH,
+      KAN,
+      KEA,
+      KEG,
+      KUS,
+      LCF,
+      LEH,
+      LIB,
+      LUZ,
+      MAS,
+      MCN,
+      MER,
+      MID,
+      MIK,
+      MIN,
+      MOD,
+      MOL,
+      MPO,
+      MVS,
+      NAH,
+      NAN,
+      NAP,
+      NAR,
+      NAS,
+      NSD,
+      OEG,
+      OGB,
+      OHA,
+      PAM,
+      PDM,
+      PHA,
+      PIT,
+      PLN,
+      POS,
+      PRD,
+      PRP,
+      PTB,
+      PTN,
+      PUK,
+      PUR,
+      QAT,
+      QUO,
+      REU,
+      RPE,
+      RTS,
+      SAE,
+      SAN,
+      SAO,
+      SAP,
+      SCK,
+      SGM,
+      SHB,
+      SOA,
+      SPK,
+      SRL,
+      STO,
+      SYO,
+      TAN,
+      TDC,
+      TIL,
+      TOY,
+      TRI,
+      TRN,
+      UNK,
+      VOI,
+      VOR,
+      WAK,
+      WGA,
+      WGB,
+      WGC,
+      WGE, /// World Geodetic System 1984
+      YAC, /// Yacare
+      ZAN  /// Zanderij
+    };
+
+    ///
+    enum class FreqUnitOfMeasure
+    {
+      megaHertz,
+      kiloHertz
+    };
+
+    /// This data type indicates if the latitude is north or south.
+    enum class NorthSouth
+    {
+      North,
+      South
+    };
+
+    /// This data type indicates if the longitude is east or west.
+    enum class EastWest
+    {
+      East,
+      West
+    };
+
+    /// The field has multiple definitions.
+    /// For Airport and Heliport Primary Records, it is used to indicate that all bearing and course detail for that airport are included in the data base with a reference to either Magnetic North or to True North.
+    /// The field is blank in Airport Record when the data base contains a mix of magnetic and true bearing or course information for the airport.
+    /// The Magnetic/True Indicator field is also used to indicate if the Course From and Course To fields of the Cruise Table record and the Sector Bearing fields of the MSA and TAA record are in magnetic or true degrees.
+    enum class MagneticTrueIndicator
+    {
+      Both,    ///< The field will be blank if details and procedures are provided in both magnetic and true for the airport. In such cases the individual detail or procedure records will contain the magnetic or true information.
+      True,    ///< if all detail and procedure for the airport are reported in true bearing
+      Magnetic ///< if all detail and procedure for the airport are reported in magnetic bearing
+    };
+
+    /// The Magnetic Variation field specifies the angular difference between True North and Magnetic North at the location defined in the record.
+    /// Dynamic Magnetic Variation is a computer model derived value and takes location and date into consideration.
+    /// For the Station Declination used in some record types, refer to Section 5.66.
+    enum class MagneticVariationEWT
+    {
+      East, ///< Magnetic variation is East of TRUE North
+      West, ///< Magnetic variation is West of TRUE North
+      True  ///< The element defined in the current record is provided TRUE.
+    };
+
+    /// This data type provides information on weather information that is broadcast from this navaid.
+    enum class NavaidWeatherInfo
+    {
+      AUTOMATED, ///< Automatic Transcribed Weather. The frequency of this Navaid is used for the continuous broadcast of some sort of automated weather system such as AWOS, ASOS, TWEB, AWIB, AWIS.
+      SCHEDULED  ///< Broadcast Scheduled Weather. The frequency of this Navaid is used for the scheduled, noncontinuous broadcast of some sort of automated weather system such as VOLMET.
+    };
+
+    /// The field NDBNavaidCoverage is the first character of the 5 character Navaid class, when used in NDB Navaid Record – NDBs and Terminal NDBs.
+    enum class NdbNavaidCoverage
+    {
+      HighPowerNdb, ///< High-powered NDB. Generally usable within 75NM of the facility at all altitudes.
+      LowPowerNdb,  ///< Low-powered NDB. Generally usable within 25NM of the facility at all altitude.
+      Locator,      ///< Locator. Generally usable within 15NM of the facility at all altitudes.
+      NDB           ///< NDB. Generally usable within 50NM of the facility at all altitude.
+    };
+
+    /// The field NDBNavaidFacility2 is the second character of the 5 character Navaid class, when used in NDB Navaid Record – NDBs and Terminal NDBs.
+    enum class NdbNavaidIfMarkerInfo
+    {
+      InnerMarker,  ///< Inner Marker. There is an Inner Marker beacon at this location.
+      MiddleMarker, ///< Middle Marker. There is a Middle Marker beacon at this location.
+      OuterMarker,  ///< Outer Marker. There is an Outer Marker beacon at this location.
+      BackMarker    ///< Back Marker. There is a Backcourse Marker at this location.
+    };
+
+    /// The field NDBNavaidFacility1 is the first character of the 5 character Navaid class, when used in NDB Navaid Record – NDBs and Terminal NDBs.
+    /// It indicates the type of navaid represented by the NDB.
+    enum class NdbNavaidType
+    {
+      Ndb,
+      Sabh,
+      MarineBeacon
+    };
+
+    /// Airports can be classified into four categories, airports open to the general public, military airports, joint use civil and military, and airports closed to the public.
+    /// This field permits these airports to be categorized by their use.
+    enum class PublicMilitaryIndicator
+    {
+      Private,  ///< Airport/Heliport is not open to the public
+      Civil,    ///< Airport/Heliport is open to the public
+      Military, ///< Airport/Heliport is military airport
+      Joint     ///< Airport/Heliport is joint Civil and Military
+    };
+
+    /// The Facility Characteristics field identifies the characteristics of the NAVAID facility.
+    enum class TypeOfEmission
+    {
+      type400,
+      type1020
+    };
+
+  } // namespace Enum
 } // namespace Arinc424
